cb83e6e571af84b3ac7ba40e433a8f59a1e994fd
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-03-13 18:25:23 +0900

    add bootstrap to flexbox.html and fix some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
 gulp.task('watch', function () {
-  gulp.watch("css/.*css", browserSync.reload);
-  gulp.watch("js/.*js", browserSync.reload);
-  gulp.watch("img/.*.{png,jpg,svg}", browserSync.reload);
-  gulp.watch("index.html", browserSync.reload);
+  gulp.watch("css/*.css", browserSync.reload);
+  gulp.watch("js/*.js", browserSync.reload);
+  gulp.watch("img/*.{png,jpg,svg}", browserSync.reload);
+  gulp.watch("*.html", browserSync.reload);
 });
 
 gulp.task('browser-sync', function() {
