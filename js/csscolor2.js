1cca43de37a29db6ecc2d5b9541af3ed22b6bd5c
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-03-27 05:28:51 +0900

    syntax organizer

--- js/csscolor2.js
+++ js/csscolor2.js
@@ -1,17 +1,19 @@
-var showSpan = function(parentElement){
+var showSpan = function (parentElement) {
   parentElement.removeChild(parentElement.children[1]);
   var spanElement = parentElement.children[0];
   spanElement.removeAttribute("hidden");
 
 }
 
-function showInput(parentElement, awesomParams){
+function showInput(parentElement, awesomParams) {
   //Just exist if focus -> click on the same input element
-  if(parentElement.children.length > 1)
+  if (parentElement.children.length > 1)
     return;
   else {
     var inputElement = document.createElement("input");
-    inputElement.addEventListener("blur", function(){ showSpan(parentElement); });
+    inputElement.addEventListener("blur", function () {
+      showSpan(parentElement);
+    });
     inputElement.className = "css-editor-input";
     parentElement.appendChild(inputElement);
     new Awesomplete(inputElement, awesomParams);
@@ -23,20 +25,20 @@ function showInput(parentElement, awesomParams){
   }
 }
 
-function showProperty(parentElement){
-  showInput(parentElement, { list: properties, minChars: 1 } )
+function showProperty(parentElement) {
+  showInput(parentElement, {list: properties, minChars: 1})
 }
 
-function showColor(parentElement){
-  showInput(parentElement, { list: colors, minChars: 1, item: colorElement } )
+function showColor(parentElement) {
+  showInput(parentElement, {list: colors, minChars: 1, item: colorElement})
 }
 
-function colorElement(text, input){
-  var split     = text.split(",");
-  var colorHex  = split[0]
+function colorElement(text, input) {
+  var split = text.split(",");
+  var colorHex = split[0]
   var colorName = split[split.length - 1];
   var element = document.createElement("li");
-  element.innerHTML = colorHex + " " + colorName ;
+  element.innerHTML = colorHex + " " + colorName;
   element.style.backgroundColor = colorHex;
   return element;
 };
@@ -44,7 +46,7 @@ function colorElement(text, input){
 var colorAjax = new XMLHttpRequest();
 var colors = "";
 colorAjax.open("GET", "data/awesomplete-colors.json", true);
-colorAjax.onload = function() {
+colorAjax.onload = function () {
   colors = JSON.parse(colorAjax.responseText);
 };
 colorAjax.send();
@@ -52,7 +54,7 @@ colorAjax.send();
 var propertyAjax = new XMLHttpRequest();
 var properties = "";
 propertyAjax.open("GET", "data/awesomplete-properties.json", true);
-propertyAjax.onload = function() {
+propertyAjax.onload = function () {
   properties = JSON.parse(propertyAjax.responseText);
 };
 propertyAjax.send();
