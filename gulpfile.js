b3e164522a05ee0441eab24e1b0a3a5fc07ddb89
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-04-02 23:43:27 +0900

    Add browserify, babelify, and redux

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,45 @@
 var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+
+var glob        = require('glob');
 var browserSync = require('browser-sync').create();
+
+var browserify  = require('browserify');
+var babelify    = require('babelify');
+var watchify    = require('watchify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var sourcemaps  = require('gulp-sourcemaps');
+var assign      = require('lodash.assign');
+
+// add custom browserify options here
+var customOpts = {
+  entries: glob.sync('./js/es6/*.js'),
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts)).transform("babelify", {presets: ["es2015"]});
+
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+      .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+      .pipe(source('bundle.js'))
+    // optional, remove if you don't need to buffer file contents
+      .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+      .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+    // Add transformation tasks to the pipeline here.
+      .pipe(sourcemaps.write('./')) // writes .map file
+      .pipe(gulp.dest('./dist'));
+}
 
 gulp.task('watch', function () {
   gulp.watch("css/*.css", browserSync.reload);
@@ -18,4 +58,4 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch', 'js']);
