998584f0ad20513c282d4a154aa8c026a3b5152d
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-04-09 23:57:28 +0900

    Pretty Print function

--- js/es6/prettyprint.js
+++ js/es6/prettyprint.js
@@ -1,3 +1,8 @@
-export default function prettyPrint(immutableMap){
-  console.log( JSON.stringify(immutableMap.toJS(), null, " " ) )
+import {Map} from 'immutable'
+
+export default function prettyPrint(jsonObj){
+  if(Map.isMap(jsonObj))
+    console.log( JSON.stringify(jsonObj.toJS(), null, " " ) );
+  else
+    console.log( JSON.stringify(jsonObj, null, " " ) )
 }
