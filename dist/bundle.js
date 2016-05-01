a6849bf70be61023d0a48cc57a98f1cac234bcf8
Richard S. Imaoka <richard.s.imaoka@gmail.com>
2016-05-01 16:47:14 +0900

    Progress bar to be 0% to 100% only

--- dist/bundle.js
+++ dist/bundle.js
@@ -1650,13 +1650,18 @@ function navigation() {
 
   switch (action.type) {
     case _navigationActions.NEXT_STEP:
-      console.log("action received", action);
-      return { currentStep: state.currentStep + 1, totalSteps: state.totalSteps };
+      console.debug("action received", action);
+      if (state.currentStep >= state.totalSteps) {
+        console.info("Not going to the next step: Total steps = ", state.totalSteps, ", Current step = ", state.currentStep, " already.");
+        return state;
+      } else return { currentStep: state.currentStep + 1, totalSteps: state.totalSteps };
     case _navigationActions.PREV_STEP:
-      console.log("action received", action);
-      return { currentStep: state.currentStep - 1, totalSteps: state.totalSteps };
+      if (state.currentStep <= 0) {
+        console.debug("Not going to the prev step: Current step = ", state.currentStep);
+        return state;
+      } else return { currentStep: state.currentStep - 1, totalSteps: state.totalSteps };
     case _navigationActions.SET_TOTAL_STEPS:
-      console.log("action received", action);
+      console.debug("action received", action);
       return { currentStep: state.currentStep, totalSteps: action.totalSteps };
     default:
       //console.log("HTML: undefined action received", action);
--- js/reducers/navigationReducer.js
+++ js/reducers/navigationReducer.js
@@ -4,13 +4,22 @@ import { NEXT_STEP, PREV_STEP, SET_TOTAL_STEPS } from '../actions/navigationActi
 export function navigation(state = { currentStep: 0, totalSteps: 1 }, action = undefined){
   switch(action.type) {
     case NEXT_STEP:
-      console.log("action received", action);
-      return { currentStep: state.currentStep + 1, totalSteps: state.totalSteps };
+      console.debug("action received", action);
+      if(state.currentStep >= state.totalSteps){
+        console.info("Not going to the next step: Total steps = ", state.totalSteps, ", Current step = ", state.currentStep, " already."  );
+        return state;
+      }
+      else
+        return { currentStep: state.currentStep + 1, totalSteps: state.totalSteps };
     case PREV_STEP:
-      console.log("action received", action);
-      return { currentStep: state.currentStep - 1, totalSteps: state.totalSteps };
+      if(state.currentStep <= 0 ){
+        console.debug("Not going to the prev step: Current step = ", state.currentStep  );
+        return state;
+      }
+      else
+        return { currentStep: state.currentStep - 1, totalSteps: state.totalSteps };
     case SET_TOTAL_STEPS:
-      console.log("action received", action);
+      console.debug("action received", action);
       return { currentStep: state.currentStep,     totalSteps: action.totalSteps };
     default:
       //console.log("HTML: undefined action received", action);
